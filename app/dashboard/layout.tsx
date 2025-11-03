93d7c2b1ca165af3258c59986f4df86f7dff82d1
afrinaldi-knitto <it17.knitto@gmail.com>
2025-11-03 11:33:16 +0700

    feat: completed app router

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import SideNav from "../ui/dashboard/sidenav";
+import { Metadata } from "next";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
